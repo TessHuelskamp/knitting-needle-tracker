e84e37f1deec34dbc517aa0391d6a9ee0e57da5e
Tess Huelskamp <tess@huelskamp.io>
2025-05-04 19:01:43 -0500

    get sticky working. works best when opening in new tab

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,8 +46,8 @@ export default function Index () {
 
   // should probs use a set but w/e
   const [library, setLibrary] = useState<Record<string, any>>({
-    "ch-0": 1,
-    "ch-10": 1,
+    "Cr-0": 1,
+    "Cr-10": 1,
     '16"-5': 1
   });
 
@@ -67,37 +67,40 @@ export default function Index () {
   }
 
 
-  const needles = ["ch", "straights", "dpn", '8"', '16"', '24"', '36"', '40"+']
-  const sizes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+  const needles = ["Cr", "str", "dpn", '8"', '16"', '24"', '36"', '40"', 'a', 'b', 'c', 'd']
+  const sizes = ['00', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
 
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
+      <div className="flex flex-col items-center gap-16 h-dvh">
         <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+          <h1 className="leading text-2xl font-bold text-gray-800 mt-4">
             Knitting Needle Tracker
           </h1>
         </header>
-        <div>
-          <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-              <th scope="col" className="px-6 py-3" />
-              {
-                needles.map(n => <th key={n} scope="col" className="px-6 py-3">{n}</th>)
-              }
-            </thead>
-            <tbody>
-              {
-                sizes.map(s => {
-                  return <tr key={s} className="bg-white border-b border-gray-300">
-                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap border-l border-gray-200 ">{s}</td>{
-                      needles.map(n => <NeedleCell key={nKey(n, s)} needleName={n} size={s} value={library[nKey(n, s)]} toggleNeedle={() => toggleValue(n, s)} />)
-                    }</tr>
-                })
-              }
-            </tbody>
-          </table>
+        <div className="">
+          {/* Table container */}
+          <div className="max-height-[80vh] border-red border m-4 p-4">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 sticky top-0">
+                <th scope="col" className="px-6 py-3" />
+                {
+                  needles.map(n => <th key={n} scope="col" className="px-6 py-3 ">{n}</th>)
+                }
+              </thead>
+              <tbody>
+                {
+                  sizes.map(s => {
+                    return <tr key={s} className="bg-white border-b border-gray-300">
+                      <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap border-l border-gray-200 text-center sticky left-0 bg-gray-50">{s}</td>{
+                        needles.map(n => <NeedleCell key={nKey(n, s)} needleName={n} size={s} value={library[nKey(n, s)]} toggleNeedle={() => toggleValue(n, s)} />)
+                      }</tr>
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
